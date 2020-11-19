0f9b60ad51a0be581f1c8ceef162e9a888de3e9b
Erick Knaebel <erick.knaebel@gmail.com>
2020-11-19 10:34:18 -0500

    Create formgroup for registration form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { Components } from '../components';
 import { Containers } from '../containers';
+
 import { NgModule } from '@angular/core';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -13,7 +18,11 @@ import { NgModule } from '@angular/core';
   ],
   imports: [
     AppRoutingModule,
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    
 
   ],
   providers: [],
--- src/components/register/register.component.ts
+++ src/components/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-register-component',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  @Output() onRegister: EventEmitter<any> = new EventEmitter<any>();
+  
+  registrationForm: FormGroup;
+
+  constructor() { 
+    this.registrationForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      emailAddress: new FormControl('', [
+        Validators.required, 
+        Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")
+      ]),
+      password: new FormControl('', Validators.required),
+      displayName: new FormControl('', Validators.required),
+    })
+  }
 
   ngOnInit() {
+  }
+
+  public submitRegistration(): void {
+    console.log('submitting registration....')
+    console.log(this.registrationForm.getRawValue())
+    this.onRegister.emit(this.registrationForm.getRawValue());
   }
 
 }
