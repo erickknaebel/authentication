079974e1b91cb517b5e724fe915c53c257577123
erickknaebel <erick.knaebel@gmail.com>
2020-12-03 10:01:03 -0500

    Feature/user authentication (#2)
    * Update to get authoriaztion token from response header.
    
    * Update register page to use firstName and lastName.

--- src/containers/login/login.container.ts
+++ src/containers/login/login.container.ts
@@ -1,23 +1,30 @@
-import { AuthService } from 'src/services/auth.service'
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { User } from 'src/interfaces/user';
+import { AuthService } from "src/services/auth.service";
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { User } from "src/interfaces/user";
+import { saveUserInfo } from "../../helpers/user.info";
 
 @Component({
-  selector: 'app-login-container',
-  template: '<app-login-component (onLogin)="submitLoginForm($event)"></app-login-component>',
-  styles: []
+  selector: "app-login-container",
+  template:
+    '<app-login-component (onLogin)="submitLoginForm($event)"></app-login-component>',
+  styles: [],
 })
 export class LoginContainer {
-
-  constructor(private _as: AuthService, private _router: Router) { }
+  constructor(private _as: AuthService, private _router: Router) {}
 
   submitLoginForm(data: User) {
-    this._as.loginUser(data).subscribe(response => {
-      localStorage.setItem('token', response);
-      this._router.navigate(['protected']);
-    }, err => {
-      console.log(err)
-    })
+    this._as.loginUser(data).subscribe(
+      (res) => {
+        saveUserInfo({
+          token: res.headers.get("Authorization"),
+          user: res.body["data"],
+        });
+        this._router.navigate(["protected"]);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,41 +1,42 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { environment } from '../environments/environment';
-import { User } from 'src/interfaces/user';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
+import { environment } from "../environments/environment";
+import { User } from "src/interfaces/user";
+import { Observable } from "rxjs";
+import { map, tap } from "rxjs/operators";
 
 @Injectable()
 export class AuthService {
-
   private _api: string = environment.databaseConfig.baseUrl;
-  private _registerUrl: string = this._api + 'users/create';
-  private _loginUrl: string = this._api + 'users/login';
+  private _registerUrl: string = this._api + "users/register";
+  private _loginUrl: string = this._api + "users/login";
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {}
 
-  registerUser(user: User): Observable<any> {
-    return this._http.post(
-      this._registerUrl,
-      JSON.stringify(user),
-      { headers: new HttpHeaders({ 'Content-Type': 'application/json' }), responseType: 'text' });
+  registerUser(user: any): Observable<any> {
+    return this._http.post(this._registerUrl, user, {
+      headers: new HttpHeaders({ "Content-Type": "application/json" }),
+      responseType: "text",
+    });
   }
 
-  loginUser(user: User): Observable<any> {
-    return this._http.post(
-      this._loginUrl,
-      JSON.stringify(user),
-      { headers: new HttpHeaders({ 'Content-Type': 'application/json' }), responseType: 'text' });
+  loginUser(user: any): Observable<HttpResponse<Object>> {
+    return this._http.post(this._loginUrl, user, { observe: "response" }).pipe(
+      map((response) => {
+        return response;
+      })
+    );
   }
 
   logoutUser(): void {
-    localStorage.removeItem('token')
+    localStorage.removeItem("token");
   }
 
   getToken() {
-    return localStorage.getItem('token')
+    return localStorage.getItem("token");
   }
 
   loggedIn() {
-    return !!localStorage.getItem('token')
+    return !!localStorage.getItem("token");
   }
 }
